0ec6116a82dc735e695d7f26eb7580ff8dede9c9
Fernando W S Furtado <feh.wilinando@gmail.com>
2018-01-11 21:32:49 -0200

    Open URL via appdelegate and URL Types

--- IP67-Swift4/AppDelegate.swift
+++ IP67-Swift4/AppDelegate.swift
@@ -17,8 +17,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
         IQKeyboardManager.sharedManager().enable = true
+        
+        return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+        guard let controller = window?.rootViewController else {
+            return false
+        }
+        
+        let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false)
+        
+        guard let items = urlComponents?.queryItems else {
+            return true
+        }
+        
+        
+        let message = items.map({ "key: \($0.name) - value: \($0.value ?? "none")" }).reduce("", {$0 + "\n" + $1})
+        
+        let alert = UIAlertController(title: "Open from Schema", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancelar", style: .cancel, handler: nil))
+        
+        controller.present(alert, animated: true, completion: nil)
         
         return true
     }
